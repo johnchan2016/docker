8b268d75f9a08dd9cc529d144f9126a83e19a6f9
johnchan <myhk2009@gmail.com>
2019-05-25 08:26:32 +0800

    add express module

---
+++ app.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var PORT = 8100;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`)
+})
