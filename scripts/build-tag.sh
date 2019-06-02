0aa65318b1f13348ad6e6dc9ebb77eec3d337f87
johnchan <myhk2009@gmail.com>
2019-06-02 22:16:52 +0800

    manage env

--- scripts/build-tag.sh
+++ scripts/build-tag.sh
@@ -2,7 +2,7 @@
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
 
 if [ "$version" != "" ]; then
-    git tag -a "v$version" -m "`git log -1 --format=%s`"
-    EXPORT TAG_NO="v$version"
-    echo "Created a new tag, v$version"
+    git tag -a "$version" -m "`git log -1 --format=%s`"
+    EXPORT TAG_NO="$version"
+    echo "Created a new tag, $version"
 fi
