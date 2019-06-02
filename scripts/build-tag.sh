c79e8ba10df50876bea4c87a8dda0b5518ae8841
johnchan <myhk2009@gmail.com>
2019-06-02 08:40:43 +0800

    add scripts

---
+++ scripts/build-tag.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+
+if [ "$version" != "" ]; then
+    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    EXPORT TAG_NO="v$version"
+    echo "Created a new tag, v$version"
+fi
