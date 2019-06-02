f7b736a7dcaae94916244e4b2da94088096d76d8
johnchan <myhk2009@gmail.com>
2019-06-02 22:41:47 +0800

    add tag_no variable

--- scripts/build-tag.sh
+++ scripts/build-tag.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
 
-if [ "$version" != "" ]; then
+if [ "$version" != "" ]; 
+then
     git tag -a "$version" -m "`git log -1 --format=%s`"
     EXPORT TAG_NO="$version"
     echo "Created a new tag, $version"
+else
+    EXPORT TAG_NO=node -p "require('./package.json').version"
+    exit 1
 fi
